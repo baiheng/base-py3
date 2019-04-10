9655960af38f7fa74001a84a6736dc914f27588b
bryant <465299904@qq.com>
2019-04-10 14:14:09 +0800

    Update settings_production.py

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -11,9 +11,9 @@ class BaseConfig(object):
     # 生产新数据库
     MYSQL_HOST = "172.16.99.73"
     MYSQL_PORT = 3306
-    MYSQL_USER = "jpush"
-    MYSQL_PWD = "JPUSH_Ras2015"
-    MYSQL_DB = "devops"
+    MYSQL_USER = "**"
+    MYSQL_PWD = "**"
+    MYSQL_DB = "**"
 
     # 3小时后自动重连mysql
     MYSQL_CONNECT_TIMEOUT = 3 * 24 * 60 * 60
